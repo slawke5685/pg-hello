1bcb9a8bbf91ec6a8e9a84b6645f0df38ec4bf04
s-115-27 <s-115-27@gumiforest.ae.krakow.pl>
2018-03-05 17:10:28 +0100

    wszystko

---
+++ www/js/personal.js
@@ -0,0 +1,22 @@
+function init() {
+	document.addEventListener("deviceready",onDeviceReady, false);
+}
+
+function onDeviceReady() {
+	navigator.notification.beep(1);
+}
+
+function deviceInfo() {
+
+	info =  'Hi, I am your smartphone :-)' + '\n' +
+			'=====' + '\n' +
+			'Imie    : '     + Slawek     + '\n' + 
+			'Nazwisko : '  + Zdzylowski + '\n' + 
+			'NrIndeksu: ' + 188846 + '\n' + 
+			'Device UUID    : '     + device.uuid     + '\n' + 
+			'Device Model   : '    + device.model     + '\n' + 
+			'Device Version : '  + device.version  + '\n';
+
+	navigator.notification.alert(info);
+	
+}
